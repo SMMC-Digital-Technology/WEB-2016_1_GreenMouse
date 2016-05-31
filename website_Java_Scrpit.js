4bd1d3cdedc7b47c328246348ea9f2f740ecc58d
JonathanNemec <NEMECJA@mackillop.act.edu.au>
2016-05-31 12:56:33 +1000

    Js tax amount works
    I changed some of the html so that the java fits, and worked with the
    java so that it get your tax.

--- website_Java_Scrpit.js
+++ website_Java_Scrpit.js
@@ -1,18 +1,23 @@
-var income = 0;
-
-if (income <= 16000){
-  var taxAmount = 0;
-  console.log(taxAmount);
-} else if (income <= 52500) {
-  var taxAmount = (income - 16000) * 0.1;
-  console.log(taxAmount);
-} else if (income <= 113000) {
-var taxAmount = ((income - 52500) * 0.16) + 3650;
-  console.log(taxAmount);
-} else if (income <= 184500) {
-var taxAmount = ((income - 113000) * 0.24) + 13330;
-  console.log(taxAmount);
-} else {
-  var taxAmount = ((income - 184500) * 0.36)         + 30490;
-  console.log(taxAmount);
+function caculateTax() {
+    var incomeInput = document.getElementById("incomeInput");
+    var I = incomeInput.value;
+    var taxOutput = document.getElementById("taxOutput");
+    var T = incomeInput.value;
+    
+    if (I <= 16000){
+        T = 0;
+        tax.innerHTML = "$" + T;
+    } else if (I <= 52500) {
+        T = (T - 16000) * 0.1;
+        tax.innerHTML = "$" + T;
+    } else if (I <= 113000) {
+        T = ((T - 52500) * 0.16) + 3650;
+        tax.innerHTML = "$" + T;
+    } else if (I <= 184500) {
+        T = ((T - 113000) * 0.24) + 13330;
+        tax.innerHTML = "$" + T;
+    } else {
+        T = ((T - 184500) * 0.36) + 30490;
+        tax.innerHTML = "$" + T;
+    }
 }
