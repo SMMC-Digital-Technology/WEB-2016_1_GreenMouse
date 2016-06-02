41d78108daa9b0d66a373b3fed03424525f508d4
JonathanNemec <NEMECJA@mackillop.act.edu.au>
2016-06-02 21:30:07 +1000

    Graph comments
    The graph now contines commentsm,plotty have 50545 line of coading and
    will not be commented on.

--- Graph.js
+++ Graph.js
@@ -2,43 +2,52 @@
     return principle * rate * time;
     }
 
-    /**
-    * This function draws a graph
+    /*
+    * This funtion will start the proccess that draws the graph
     */
     function drawGraph() {
 
-        // declare all variables (variables inside a function are created when the function is called)
+        /*
+        *creates all the needed varables
+        */
         var trace, data, layout;
 
-        /* Trace is an object that stores the x and y values. The x, y and type variables are required mandatory. */
+        /* 
+        *sets the trace scores of the x and y value. Also sets the type of graph and how the data should be displayed.
+        */
         trace = {
-            x: [0, 16000, 34250, 52500, 82750, 113000, 148750, 184500, 217250, 250000], // x values
-            y: [0, 0, 1825, 3650, 8490, 13330, 21910, 30490, 42280, 54070], // y values
-            type: 'scatter', // the type of graph
-            mode: 'lines+markers', // how the data should be displayed
-            name: "trace" // a name for the data to appear on the legend (if present)
+            x: [0, 16000, 34250, 52500, 82750, 113000, 148750, 184500, 217250, 250000], 
+            y: [0, 0, 1825, 3650, 8490, 13330, 21910, 30490, 42280, 54070], 
+            type: 'scatter', 
+            mode: 'lines+markers', 
+            name: "trace" 
         };
 
-        // Data is an array that stores the traces we wish to graph. Add further traces to the array for multiple data sets.
         data = [trace];
 
-        /* To add a title and axis labels, we need to create a layout (another object). Setting a layout is optional, but they do improve the graph. */
+        /* 
+        *Adds a title to the x and y axis, and sets the widths of the zero lines. 
+        */
         layout = {
             title: "Tax Amount Average",
             yaxis: {
-                title: "Tax Amount", // give the axis a label
-                zerolinewidth: 1.5 // makes the zero line thicker
+                title: "Tax Amount", 
+                zerolinewidth: 1.5 
             },
             xaxis: {
                 title: "Income Amount",
                 zerolinewidth: 1.5,
-                range: [0, 250000] // sets a minimum and maximum value for the axis
+                range: [0, 250000] 
             }
         };
 
-        /* Finally we tell plotly to put the graph in the 'graph' div.*/
+        /* 
+        *Gives plotty the ok to draw the graph
+        */
         Plotly.newPlot("graph", data, layout);
     }
 
-    // Run the function to draw the graph.
+    /*
+    *run the function that will draw the graph
+    */
     drawGraph();
